5ebb6109552705486ae4080b8354d929d15b6884
Chiharu Kawatake <chiharu.kawatake@gmail.com>
2017-07-24 08:45:50 +0900

    chore: commit temporarily

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,26 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  Kp_ = Kp;
+  Ki_ = Ki;
+  Kd_ = Kd;
+  
+  p_error_ = 0;
+  i_error_ = 0;
+  d_error_ = 0;
+  
+  previous_cte_ = 0;
 }
 
 void PID::UpdateError(double cte) {
+  p_error_ = cte;
+  i_error_ += cte;
+  d_error_ = cte - previous_cte_;
+  
+  previous_cte_ = cte;
 }
 
 double PID::TotalError() {
+  return -(Kp_ * p_error_ + Ki_ * i_error_ + Kd_ * d_error_);
 }
 
